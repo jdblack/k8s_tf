2cbbe3b3d72106ff5de12b7fb032bf5a3aad1fb3
James Blackwell <jblack@linuxguru.net>
2025-01-18 14:14:04 +0700

    fix flapping themes

--- deploy_mantle/auth/keycloak/realm.tf
+++ deploy_mantle/auth/keycloak/realm.tf
@@ -3,14 +3,20 @@ resource "keycloak_realm" "realm" {
   enabled           = true
   display_name      = var.realm_display
   display_name_html = "<b>${var.realm_display}</b>"
-  login_theme = "base"
-  admin_theme = "base"
   access_token_lifespan = "1h"
   access_token_lifespan_for_implicit_flow = "1h"
   sso_session_idle_timeout = "24h"
   sso_session_idle_timeout_remember_me = "336h"
   sso_session_max_lifespan = "48h"
   sso_session_max_lifespan_remember_me = "720h"
+  lifecycle {
+    ignore_changes = [
+      account_theme,
+      email_theme,
+      admin_theme,
+      login_theme
+    ]
+  }
 }
 
 resource "keycloak_group" "clusteradmin" {
