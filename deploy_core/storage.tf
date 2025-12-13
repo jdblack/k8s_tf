82645be8a7d901974445811a7140540670fc99c3
James Blackwell <jblack@linuxguru.net>
2025-12-14 00:29:24 +0700

    we now have s3

--- deploy_core/storage.tf
+++ deploy_core/storage.tf
@@ -9,3 +9,8 @@ module juicefs {
   namespace = "juicefs"
 }
 
+module seaweedfs {
+  source = "../modules/storage/seaweedfs"
+  namespace = "kube-storage"
+  cert_issuers = var.deployment["cert_authorities"]
+}
---
+++ modules/storage/seaweedfs/variables.tf
@@ -0,0 +1,83 @@
+variable namespace { type = string }
+variable cert_issuers { type = map }
+variable name { default = "seaweedfs" }
+variable config_size { default = "1Gi" }
+variable helm_repo { default = "https://seaweedfs.github.io/seaweedfs/helm" }
+variable chart { default = "seaweedfs" }
+variable visibility { default = "private" }
+
+locals {
+  sub = var.visibility == "private" ? ".vn" : "" 
+  issuer = var.cert_issuers[var.visibility]
+
+  fqdn = "${var.name}${local.sub}.linuxguru.net"
+  master_host = "master.${local.fqdn}"
+  filer_host = "filer.${local.fqdn}"
+  s3_host = "s3.vn.linuxguru.net"
+
+  helm_values = {
+    global = {
+      enableReplication = true
+      replicationPlacement = "001"
+    }
+    master = {
+      replicas = 1
+      data = {
+        type = "persistentVolumeClaim"
+        size = "2Gi"
+        storageClass = ""
+      }
+      ingress = {
+        enabled = true
+        path = "/"
+        host = local.master_host
+        className = var.visibility
+        annotations = {
+          "cert-manager.io/cluster-issuer" = local.issuer,
+          "external-dns.alpha.kubernetes.io/hostname" = local.master_host,
+        }
+      }
+    }
+    volume = {
+      replicas = 6
+      rack = "0"
+      dataCenter = "linuxguru-hcmc"
+      idx = {
+        type = "persistentVolumeClaim"
+        size = "2Gi"
+        storageClass = ""
+      }
+      data = {
+        type = "hostPath"
+        storageClass = ""
+        hostPathPrefix = "/ssd"
+      }
+    }
+    filer = {
+      replicas = 2
+      data = {
+        type = "persistentVolumeClaim"
+        size = "2Gi"
+        storageClass = ""
+      }
+    }
+    s3 = {
+      enabled = true
+      enableAuth = true
+      domain_name = local.s3_host
+      host = local.s3_host
+      ingress = {
+        enabled = true
+        className = var.visibility
+        host = local.s3_host
+        path = "/"
+        annotations = {
+          "nginx.ingress.kubernetes.io/proxy-body-size" = "0",
+          "cert-manager.io/cluster-issuer" = local.issuer,
+          "external-dns.alpha.kubernetes.io/hostname" = local.s3_host
+        }
+      }
+
+    }
+  }
+}
