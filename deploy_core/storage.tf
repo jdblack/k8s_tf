7d29e87e219431b7a8c5fce3f450d47d42c56e6e
James Blackwell <jblack@linuxguru.net>
2026-01-11 16:57:52 +0700

    cleaned up storage

--- deploy_core/storage.tf
+++ deploy_core/storage.tf
@@ -1,15 +1,4 @@
 
-#module minio {
-#  source = "../modules/storage/minio"
-#  namespace = "kube-storage"
-#}
-
-module juicefs {
-  count = 0
-  source = "../modules/storage/juicefs"
-  namespace = "juicefs"
-}
-
 module seaweedfs {
   source = "../modules/storage/seaweedfs"
   namespace = "kube-storage"
--- modules/storage/old/csi-s3.tf
+++ modules/storage/old/csi-s3.tf
@@ -1,22 +1,19 @@
+variable  namespace {  default = "kube-storage" }
+
 locals {
   csi_s3_config = {
-    "storageClass.mounter" = "s3fs"
-    "storageClass.mountOptions" = "-o allow_other  -o umask=0222"
+    storageClass = {
+      mounter = "s3fs"
+      mountOptions = "-o allow_other  -o umask=0222"
+    }
   }
 }
 
 resource helm_release csi_s3 {
-  count = 0
   name = "csi-s3"
   namespace = var.namespace
   repository = "https://yandex-cloud.github.io/k8s-csi-s3/charts"
   version = "0.43.2"
   chart =  "csi-s3"
-  set = [
-    for key, value in local.csi_s3_config : {
-      name  = key
-      value = value
-    }
-  ]
-
+  values = [yamlencode(local.prometheus)]
 }
